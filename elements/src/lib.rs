78759394133132bee7b13f8c0e8d2ccd235c9031
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-10-27 17:35:25 +0100

    Solution to the elements

--- elements/src/lib.rs
+++ elements/src/lib.rs
@@ -2,22 +2,29 @@ use hdk3::prelude::*;
 
 // Returns the header hash from the element
 pub fn get_header_hash(element: Element) -> HeaderHash {
-    unimplemented!()
+    element.header_address().clone()
 }
 
 // Returns the header timestamp from the element
 pub fn get_header_timestamp(element: Element) -> timestamp::Timestamp {
-    unimplemented!()
+    element.header().timestamp()
 }
 
 // Returns whether the element contains some entry or not
 pub fn contains_entry(element: Element) -> bool {
-    unimplemented!()
+    element.header().entry_data().is_some()
 }
 
 // Returns whether the header has been produced by the subconscious of holochain or if it was some hdk call
 pub fn is_header_subconscious(header: Header) -> bool {
-    unimplemented!()
+    match header {
+        Header::Create(_)
+        | Header::CreateLink(_)
+        | Header::Delete(_)
+        | Header::Update(_)
+        | Header::DeleteLink(_) => false,
+        _ => true,
+    }
 }
 
 /** Tests: DO NOT MODIFY */
