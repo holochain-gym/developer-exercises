203a364e4a90121cc067790b492e9e0a8419ae5c
tixel <tim@pragmaticmind.com>
2021-02-18 19:16:51 +0100

    remove solution from entries exercise

--- basic/0.entries/zomes/exercise/src/lib.rs
+++ basic/0.entries/zomes/exercise/src/lib.rs
@@ -1,9 +1,6 @@
-use hdk3::prelude::*;
 
-entry_defs![Greeting::entry_def()];
 
-#[hdk_entry()]
-pub struct Greeting(String);
+
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct SomeExternalInput {
@@ -12,6 +9,5 @@ pub struct SomeExternalInput {
 
 #[hdk_extern()]
 pub fn say_greeting(input: SomeExternalInput) -> ExternResult<HeaderHash> {
-    let greeting:Greeting = Greeting(input.content);
-    create_entry(greeting)
+    unimplemented!()
 }
