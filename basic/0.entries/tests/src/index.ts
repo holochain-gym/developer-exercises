7e53490fe1b1bd2c696adc8a59e1967f2de3fcd8
tixel <tim@pragmaticmind.com>
2021-02-18 18:12:21 +0100

    adding solutino for entries exercise. Tests are in npm, so it's a real integration test

---
+++ basic/0.entries/tests/src/index.ts
@@ -0,0 +1,43 @@
+import { Orchestrator, Config, InstallAgentsHapps } from "@holochain/tryorama";
+
+const conductorConfig = Config.gen();
+
+// Construct proper paths for your DNAs
+const exercise = path.join(__dirname, "../../exercise.dna.gz");
+
+// create an InstallAgentsHapps array with your DNAs to tell tryorama what
+// to install into the conductor.
+const installation: InstallAgentsHapps = [
+  // agent 0
+  [
+    // happ 0
+    [exercise],
+  ],
+];
+
+const sleep = (ms) =>
+  new Promise((resolve) => setTimeout(() => resolve(null), ms));
+
+const orchestrator = new Orchestrator();
+
+orchestrator.registerScenario(
+  "say a greeting",
+  async (s, t) => {
+    const [alice] = await s.players([conductorConfig]);
+
+    // install your happs into the coductors and destructuring the returned happ data using the same
+    // array structure as you created in your installation array.
+    const [[alice_common]] = await alice.installAgentsHapps(installation);
+
+    let entryHash = await alice_common.cells[0].call(
+      "exercise",
+      "say_greeting",
+      {
+        content: "Hello World",
+      }
+    );
+    t.ok(entryHash);
+  }
+);
+
+orchestrator.run();
