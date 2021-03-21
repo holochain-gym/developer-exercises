233e34c892b78702b0e6d4879d380d85d49c7db6
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-03-21 17:47:03 +0100

    Upgraded to latest core deps

--- basic/3.elements/default.nix
+++ basic/3.elements/default.nix
@@ -1,18 +1,19 @@
 let 
   holonixPath = builtins.fetchTarball {
-    url = "https://github.com/holochain/holonix/archive/cdf1d199d5489ebc943b88e552507f1063e3e571.tar.gz";
-    sha256 = "1b5pdlxj91syg1qqf42f49sxlq9qd3qnz7ccgdncjvhdfyricagh";
+    url = "https://github.com/holochain/holonix/archive/90a19d5771c069dbcc9b27938009486b54b12fb7.tar.gz";
+    sha256 = "11wv7mwliqj38jh1gda3gd0ad0vqz1d42hxnhjmqdp037gcd2cjg";
   };
   holonix = import (holonixPath) {
     includeHolochainBinaries = true;
     holochainVersionId = "custom";
     
     holochainVersion = { 
-     rev = "8c62cb5888f491f08e6fdf13b6f3991f85a9801a";  
-     sha256 = "1kgxyfrwmga27mqywhivn0xdi6br90bavqvnd4kbrfyzbzmf8fcr";  
-     cargoSha256 = "1ix8ihlizjsmx8xaaxknbl0wkyck3kc98spipx5alav8ln4wf46s";
+     rev = "9a7219edf355a2028d38f1c1303dd5c9a76bd886";
+     sha256 = "17ij6i4jw4lqqlrrfcsf33g8a12bmhfhyqy5yzz7y2i61y8sllx4";
+     cargoSha256 = "19faydkxid1d2s0k4jks6y6plgchdhidcckacrcs841my6dvy131";
      bins = {
        holochain = "holochain";
+       hc = "hc";
      };
     };
   };
