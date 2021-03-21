f19b04d70e78eb5f849ba5ba8ccc86d0a6f62536
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-03-21 16:28:16 +0100

    Fixed solution

--- basic/0.entries/zomes/exercise/src/lib.rs
+++ basic/0.entries/zomes/exercise/src/lib.rs
@@ -2,7 +2,7 @@ use hdk::prelude::*;
 
 entry_defs![Greeting::entry_def()];
 
-#[hdk_entry()]
+#[hdk_entry(id = "greeting")]
 pub struct Greeting(String);
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -10,7 +10,7 @@ pub struct SomeExternalInput {
     content: String,
 }
 
-
+#[hdk_extern]
 pub fn say_greeting(input: SomeExternalInput) -> ExternResult<HeaderHash> {
     let greeting:Greeting = Greeting(input.content);
     create_entry(greeting)
