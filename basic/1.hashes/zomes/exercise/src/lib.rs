5362dba448f30b10aeb45bdd6f53730436aed616
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-03-21 17:29:01 +0100

    Removed hc_utils deps

--- basic/1.hashes/zomes/exercise/src/lib.rs
+++ basic/1.hashes/zomes/exercise/src/lib.rs
@@ -1,9 +1,9 @@
 use hdk::prelude::*;
-use hc_utils::WrappedEntryHash;
+use holo_hash::EntryHashB64;
 
 entry_defs![Book::entry_def()];
 
-#[hdk_entry(id="book")]
+#[hdk_entry(id = "book")]
 pub struct Book {
     title: String,
     content: String,
@@ -16,26 +16,27 @@ pub struct SomeExternalInput {
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct SomeExternalEntryHash{
+pub struct SomeExternalEntryHash {
     value: String,
 }
 
 #[hdk_extern]
-pub fn add_book(input: SomeExternalInput) -> ExternResult<WrappedEntryHash> {
+pub fn add_book(input: SomeExternalInput) -> ExternResult<EntryHashB64> {
     //unimplemented!();
-    let book:Book = Book{
-                        title: input.title,
-                        content: input.content
-                    };
+    let book: Book = Book {
+        title: input.title,
+        content: input.content,
+    };
     let _a: HeaderHash = create_entry(&book)?;
     let x: EntryHash = hash_entry(&book)?;
 
-    Ok(WrappedEntryHash(x))
+    Ok(EntryHashB64::from(x))
 }
 
 #[hdk_extern]
-pub fn get_book(hash: WrappedEntryHash) -> ExternResult<Book> {
-    let element:Element = get(hash.0, GetOptions::default())?.ok_or(WasmError::Guest(String::from("Could not find book")))?;
+pub fn get_book(hash: EntryHashB64) -> ExternResult<Book> {
+    let element: Element = get(EntryHash::from(hash), GetOptions::default())?
+        .ok_or(WasmError::Guest(String::from("Could not find book")))?;
     let bookoption: Option<Book> = element.entry().to_app_option()?;
     let book: Book = bookoption.unwrap();
     Ok(book)
