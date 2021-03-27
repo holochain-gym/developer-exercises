8b94a83c8e183ad5210b63b32d41e1a05917965f
tixel <tim@pragmaticmind.com>
2021-03-27 20:48:05 +0100

    adding headers exercise

--- basic/2.headers/zomes/exercise/src/lib.rs
+++ basic/2.headers/zomes/exercise/src/lib.rs
@@ -14,23 +14,15 @@ pub struct KitchenJarLabelUpdate{
 
 #[hdk_extern]
 pub fn add_label(input: KitchenJarLabel) -> ExternResult<HeaderHashB64> {
-    let a: HeaderHash = create_entry(&input)?;
-    Ok(HeaderHashB64::from(a))
+    unimplemented!
 }
 
 #[hdk_extern]
 pub fn get_label(hash: HeaderHashB64) -> ExternResult<KitchenJarLabel> {
-    let element: Element = get(HeaderHash::from(hash), GetOptions::default())?
-        .ok_or(WasmError::Guest(String::from("Could not find KitchenJarLabel")))?;
-    let option: Option<KitchenJarLabel> = element.entry().to_app_option()?;
-    let label: KitchenJarLabel = option.unwrap();
-    Ok(label)
+    unimplemented!()
 }
 
 #[hdk_extern]
 pub fn update_label(a:KitchenJarLabelUpdate) -> ExternResult<HeaderHashB64> {
-    let header_hash: HeaderHash = HeaderHash::from(a.header_hash);
-    let new_label: KitchenJarLabel = KitchenJarLabel(a.label);
-    let header_hash_update: HeaderHash = update_entry(header_hash, new_label)?;
-    Ok(HeaderHashB64::from(header_hash_update))   
+    unimplemented!()
 }
