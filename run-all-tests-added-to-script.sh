e6a2ae1f971a5f47a261c3b16ad5a9456b6ada9c
tixel <tim@pragmaticmind.com>
2021-03-27 12:10:45 +0100

    removing tests for basic/3.elements

--- run-all-tests-added-to-script.sh
+++ run-all-tests-added-to-script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# basic exercises
 cd basic
+
+# 0.entries
 cd 0.entries 
 CARGO_TARGET_DIR=target cargo build --release --target wasm32-unknown-unknown 
 hc dna pack workdir
@@ -9,6 +12,7 @@ npm install
 npm test
 cd ..
 
+# 1.hashes
 cd ../1.hashes
 CARGO_TARGET_DIR=target cargo build --release --target wasm32-unknown-unknown 
 hc dna pack workdir
@@ -17,6 +21,7 @@ npm install
 npm test
 cd ..
 
+# 2.headers
 cd ../2.headers
 CARGO_TARGET_DIR=target cargo build --release --target wasm32-unknown-unknown 
 hc dna pack workdir
@@ -25,16 +30,10 @@ npm install
 npm test
 cd ..
 
-cd ../3.elements
-CARGO_TARGET_DIR=target cargo build --release --target wasm32-unknown-unknown 
-hc dna pack workdir
-cd tests
-npm install
-npm test
-cd ..
-
+# intermediate exercises
 cd ../../intermediate
 
+# 1.paths
 cd 1.paths
 CARGO_TARGET_DIR=target cargo build --release --target wasm32-unknown-unknown 
 hc dna pack workdir
