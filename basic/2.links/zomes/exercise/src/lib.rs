1f83ca4c6f929004a0cfbfb2fbba14acaa0506a6
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-04-11 16:22:49 +0200

    Fixed links exercise

--- basic/2.links/zomes/exercise/src/lib.rs
+++ basic/2.links/zomes/exercise/src/lib.rs
@@ -1,5 +1,3 @@
-use hdk::info::agent_info;
-use hdk::link::get_links;
 use hdk::prelude::*;
 
 //  1. Declare an entry data type called Post and register it with the entry_defs macro
@@ -10,8 +8,8 @@ entry_defs![Post::entry_def()];
 pub struct Post(String);
 
 //  2. Create an ExternalPostData structure:
-//    ExternalPostData:
-//      This structure can take a pair of strings for content and tag data.
+//    CreatePostInput:
+//      This structure can take a string for the content of the post.
 //      As all create_link function calls require something to be passed into
 //      the tag option, tag-less posts will need to be passed an empty string --> ''
 
@@ -26,14 +24,13 @@ pub struct CreatePostInput {
 //      Then return the EntryHash of the post.
 
 #[hdk_extern]
-pub fn create_post(external_data: ExternalPostData) -> ExternResult<EntryHash> {
+pub fn create_post(external_data: CreatePostInput) -> ExternResult<EntryHash> {
     unimplemented!()
 }
 
 //  4. get_posts_for_agent()
-//      Given the agent_latest_pubkey, find all posts with the specified tag and
+//      Given the agent_latest_pubkey, find all posts created by the given agent
 //      and return a vector of all the Post structures using get().
-//      A private function `_return_content` handles the content gathering.
 
 #[hdk_extern]
 pub fn get_posts_for_agent(link_query: String) -> ExternResult<Vec<Post>> {
