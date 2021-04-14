627eb0b5b6593935ac78b2595dd5f721861a7516
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-04-14 11:36:19 +0200

    Typos

--- basic/5.source-chain/zomes/exercise/src/lib.rs
+++ basic/5.source-chain/zomes/exercise/src/lib.rs
@@ -10,14 +10,13 @@ pub struct SnackingLog(String);
 pub struct WorkoutLog(String);
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct HeaderHashPair{
+pub struct HeaderHashPair {
     current: HeaderHashB64,
-    previous: HeaderHashB64
+    previous: HeaderHashB64,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Answer(String);
-
 
 #[hdk_extern]
 pub fn register_snacking(input: SnackingLog) -> ExternResult<HeaderHashB64> {
@@ -32,15 +31,15 @@ pub fn is_previous_header_hash(pair: HeaderHashPair) -> ExternResult<Answer> {
     // Ok(Answer(String::from("is NOT previous header")))
     // Ok(Answer(String::from("IS previous header")))
 
+    let current_header_hash_from_input: HeaderHash = HeaderHash::from(pair.current);
+    let prev_header_hash_from_input: HeaderHash = HeaderHash::from(pair.previous);
 
-    let current_header_hash_from_input:HeaderHash = HeaderHash::from(pair.current);
-    let prev_header_hash_from_input:HeaderHash = HeaderHash::from(pair.previous);
-
-    let element: Element = get(current_header_hash_from_input, GetOptions::default())?
-        .ok_or(WasmError::Guest(String::from("Could not find current element")))?;
+    let element: Element = get(current_header_hash_from_input, GetOptions::default())?.ok_or(
+        WasmError::Guest(String::from("Could not find current element")),
+    )?;
     let header: Header = element.header().clone();
-    let prev_header_option:Option<&HeaderHash> = header.prev_header();
-    let prev_header:HeaderHash;
+    let prev_header_option: Option<&HeaderHash> = header.prev_header();
+    let prev_header: HeaderHash;
 
     match Some(prev_header_option) {
         Some(a) => prev_header = a.unwrap().clone(),
@@ -48,7 +47,7 @@ pub fn is_previous_header_hash(pair: HeaderHashPair) -> ExternResult<Answer> {
     }
 
     if prev_header != prev_header_hash_from_input {
-        return Ok(Answer(String::from("is NOT previous header")))
+        return Ok(Answer(String::from("is NOT previous header")));
     }
 
     Ok(Answer(String::from("IS previous header")))
@@ -61,22 +60,21 @@ pub fn happened_before(pair: HeaderHashPair) -> ExternResult<Answer> {
     // Ok(Answer(String::from("happened before"))),
     // Ok(Answer(String::from("did NOT happen before")))
 
+    let starting_point: HeaderHash = HeaderHash::from(pair.current);
+    let potential_before: HeaderHash = HeaderHash::from(pair.previous);
 
-    let starting_point:HeaderHash = HeaderHash::from(pair.current);
-    let potential_before:HeaderHash = HeaderHash::from(pair.previous);
-
-    let mut current:Option<HeaderHash> = Some(starting_point);
+    let mut current: Option<HeaderHash> = Some(starting_point);
     while current != None {
-        let found:Option<HeaderHash> = get_previous_header(current.unwrap().clone());
+        let found: Option<HeaderHash> = get_previous_header(current.unwrap().clone());
         match found {
             Some(a) => {
                 if a == potential_before {
-                    let answer:Answer = Answer(String::from("happened before"));
-                    return Ok(answer) 
+                    let answer: Answer = Answer(String::from("happened before"));
+                    return Ok(answer);
                 } else {
                     current = Some(a.clone());
                 }
-            },
+            }
             None => current = None,
         }
     }
@@ -86,11 +84,10 @@ pub fn happened_before(pair: HeaderHashPair) -> ExternResult<Answer> {
     // add alternative solution with query, so user knows there is a simpler way, that will be explained later on
 }
 
-fn get_previous_header(a:HeaderHash) -> Option<HeaderHash> {
+fn get_previous_header(a: HeaderHash) -> Option<HeaderHash> {
+    let ext_result: ExternResult<Option<Element>> = get(a, GetOptions::default());
+    let el_option: Option<Element> = ext_result.unwrap_or(None);
 
-    let ext_result:ExternResult<Option<Element>> = get(a, GetOptions::default());
-    let el_option:Option<Element> = ext_result.unwrap_or(None);
-    
     match Some(el_option) {
         Some(a) => return Some(a.unwrap().header().clone().prev_header().unwrap().clone()),
         None => return None,
@@ -102,9 +99,10 @@ pub fn get_header_sequence_number(a: HeaderHashB64) -> ExternResult<Answer> {
     // unimplemented()
     // Ok(Answer(format!("header sequence is {}", sequence)))
 
-    let element: Element = get(HeaderHash::from(a), GetOptions::default())?
-    .ok_or(WasmError::Guest(String::from("Could not find current element")))?;
+    let element: Element = get(HeaderHash::from(a), GetOptions::default())?.ok_or(
+        WasmError::Guest(String::from("Could not find current element")),
+    )?;
     let header: Header = element.header().clone();
-    let sequence:u32 =  header.header_seq();
+    let sequence: u32 = header.header_seq();
     Ok(Answer(format!("header sequence is {}", sequence)))
 }
