7379cc977fd6dd25460589e72ff8a9034b9bfacc
tixel <tim@pragmaticmind.com>
2021-04-22 21:20:06 +0200

    fix test script

--- run-all-tests-added-to-script.sh
+++ run-all-tests-added-to-script.sh
@@ -4,19 +4,22 @@ concepts_entries=0.concepts/0.entries/example
 concepts_hashes=0.concepts/1.hashes/example
 concepts_headers=0.concepts/2.headers/example
 concepts_source_chain=0.concepts/3.source-chain/example
-concepts_dht=0.concepts/4.dht/example
-concepts_validation=0.concepts/5.validation/example
+# concepts_dht=0.concepts/4.dht/example
+# concepts_validation=0.concepts/5.validation/example
 
-basic_entries=1.basic/solutions/0.entries/solution
-basic_hashes=1.basic/solutions/1.hashes/solution
-basic_links=1.basic/solutions/2.links/solution
-basic_headers=1.basic/solutions/3.headers/solution
-basic_elements=1.basic/solutions/4.elements/solution
-basic_source_chain=1.basic/solutions/5-source-chain/solution
+# basic_zome_functions=1.basic/solution/0.zome-functions/solution
+# basic_entries=1.basic/solution/1.entries/solution
+# basic_elements=1.basic/solution/2.elements/solution
+# basic_hashes=1.basic/solution/3.hashes/solution
+# basic_links=1.basic/solution/4.links/solution
+# basic_anchors=1.basic/solution/5.anchors/solution
 
-intermediate_paths=2.intermediate/solutions/3.paths/solution
-intermediate_remote_call=2.intermediate/solutions/4.remote-call/solution
-intermediate_capability_tokens=2.intermediate/solutions/5.capability-tokens/solution
+# intermediate_query=2.intermediate/solution/0.query/solution
+# intermediate_update=2.intermediate/solution/1.update/solution
+# intermediate_delete=2.intermediate/solution/2.delete/solution
+# intermediate_paths=2.intermediate/solution/3.paths/solution
+# intermediate_remote_call=2.intermediate/solution/4.remote-call/solution
+# intermediate_capability_tokens=2.intermediate/solution/5.capability-tokens/solution
 
 ### TODO change script to use ./run_build.sh and ./run_tests.sh to make sure scripts are up to date
 
