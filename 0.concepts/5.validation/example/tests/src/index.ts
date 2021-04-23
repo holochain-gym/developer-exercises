032c3e94ae79d4ce5f444307842d0adb043a215c
tixel <tim@pragmaticmind.com>
2021-04-23 20:40:58 +0200

    add examples to concepts

--- 0.concepts/5.validation/example/tests/src/index.ts
+++ 0.concepts/5.validation/example/tests/src/index.ts
@@ -1,4 +1,9 @@
-import { Orchestrator, Config, InstallAgentsHapps } from "@holochain/tryorama";
+import {
+  Orchestrator,
+  Config,
+  InstallAgentsHapps,
+  Player,
+} from "@holochain/tryorama";
 import path from "path";
 
 const conductorConfig = Config.gen();
@@ -14,6 +19,10 @@ const installation: InstallAgentsHapps = [
     // happ 0
     [exercise],
   ],
+  [
+    // happ 0
+    [exercise],
+  ],
 ];
 
 const sleep = (ms) =>
@@ -21,6 +30,40 @@ const sleep = (ms) =>
 
 const orchestrator = new Orchestrator();
 
-// THIS EXAMPLE IS NOT FINISHED YET
+orchestrator.registerScenario(
+  "Make some links with tags and test retrieval",
+  async (s, t) => {
+    const [alice]: Player[] = await s.players([conductorConfig]);
+
+    const [[alice_common], [bob_common]] = await alice.installAgentsHapps(
+      installation
+    );
+
+    // Alice add a correct estimate for an item
+    let entry_hash1 = await alice_common.cells[0].call("exercise", "add_estimate", {
+      item: "work-item-xyz",
+      value: 13,
+    });
+
+    t.ok(entry_hash1);
+
+    await sleep(100);
+
+    // Alice adds a incorrect estimate for an item
+    try{
+      await alice_common.cells[0].call("exercise", "add_estimate", {
+        item: "work-item-xyz",
+        value: 6,
+      });
+      t.fail();
+    } catch(e){
+      t.ok("error thrown")
+    }
+    
+   // TODO find out if there is a way to show validation is done by peers as well
+  
+  
+  }
+);
 
 orchestrator.run();
