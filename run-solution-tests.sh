a5f11da7abb7dfffdfa765444171e9df19111174
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-05-07 19:09:53 +0200

    Fixed script

--- run-solution-tests.sh
+++ run-solution-tests.sh
@@ -4,8 +4,9 @@ cd 1.basic
 
 for D in *; do
     if [ -d "${D}" ]; then
+        echo Testing ${D}
         cd ${D}/solution && CARGO_TARGET_DIR=target cargo build --release --target wasm32-unknown-unknown && hc dna pack workdir && cd tests && npm install && npm test
-        cd ..
+        cd ../../..
     fi
 done
 
@@ -13,6 +14,7 @@ cd ../2.intermediate
 
 for D in *; do
     if [ -d "${D}" ]; then
+        echo Testing ${D}
         cd ${D}/solution && CARGO_TARGET_DIR=target cargo build --release --target wasm32-unknown-unknown && hc dna pack workdir && cd tests && npm install && npm test
         cd ..
     fi
