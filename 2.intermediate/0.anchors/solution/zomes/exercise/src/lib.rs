5df64f89a2a2504a27ba1b0a8e2a055296c83ac6
0phelia36 <0phelia36@protonmail.com>
2021-05-30 18:28:03 -0400

    Solution for anchors exercise

--- 2.intermediate/0.anchors/solution/zomes/exercise/src/lib.rs
+++ 2.intermediate/0.anchors/solution/zomes/exercise/src/lib.rs
@@ -1,9 +1,12 @@
 use hdk::prelude::*;
 
-entry_defs![Greeting::entry_def()];
+entry_defs![Post::entry_def(), Anchor::entry_def()];
 
-#[hdk_entry(id = "greeting")]
-pub struct Greeting(String);
+const POST_ANCHOR_TYPE: &str = "posts";
+const POST_ANCHOR_TEXT: &str = "posts";
+
+#[hdk_entry(id = "post")]
+pub struct Post(String);
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct SomeExternalInput {
@@ -11,7 +14,37 @@ pub struct SomeExternalInput {
 }
 
 #[hdk_extern]
-pub fn say_greeting(input: SomeExternalInput) -> ExternResult<HeaderHash> {
-    let greeting: Greeting = Greeting(input.content);
-    create_entry(greeting)
+pub fn create_post(input: SomeExternalInput) -> ExternResult<HeaderHash> {
+    let anchor = anchor(POST_ANCHOR_TYPE.into(), POST_ANCHOR_TEXT.into())?;
+    
+    let post: Post = Post(input.content);
+
+    let post_header = create_entry(&post)?;
+    let post_entry = hash_entry(&post)?;
+    create_link(anchor.into_hash(), post_entry, ())?;
+
+    Ok(post_header)
+}
+
+#[hdk_extern]
+pub fn get_all_posts(_: ()) -> ExternResult<Vec<Post>> {
+    let anchor = anchor(POST_ANCHOR_TYPE.into(), POST_ANCHOR_TEXT.into())?;
+    let mut content: Vec<Post> = Vec::new();
+
+    let links: Links = get_links(anchor.into_hash(), None)?;
+
+    for l in links.into_inner() {
+        content.push(_return_content(l)?);
+    }
+
+    Ok(content)
+}
+
+fn _return_content(link: Link) -> ExternResult<Post> {
+    let element: Element = get(link.target, GetOptions::default())?
+        .ok_or(WasmError::Guest(String::from("Entry not found")))?;
+    let entry_option: Option<Post> = element.entry().to_app_option()?;
+    let entry: Post =
+        entry_option.ok_or(WasmError::Guest("The targeted entry is not a post".into()))?;
+    Ok(entry)
 }
